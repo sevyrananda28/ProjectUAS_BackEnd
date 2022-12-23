0aa32c5c20cb654850919af07bef53656d5d92b2
sevyra <sevyra02@gmail.com>
2022-12-23 12:42:15 +0700

    add migration create table mahasiswa

---
+++ database/migrations/2022_03_10_142203_create_mahasiswa_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('nim');
+            $table->string('alamat');
+            $table->string('prodi');
+            $table->string('email');
+            $table->string('ttl');
+            $table->string('jk');
+            $table->integer('totalsks');
+            $table->integer('perolehansks');
+            $table->integer('ipk');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
